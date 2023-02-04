cafbe7341f14fccd7c47e47049aec9e127db8ccb
Icaro Luiz Albar Eduardo <icaro.albar@darede.com.br>
2023-02-04 20:26:50 -0300

    react router dom configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import { Home } from "./components/pages/Home";
+import { Register } from "./components/pages/Auth/Register";
+import { Login } from "./components/pages/Auth/Login";
 
 function App() {
   return (
-    <></>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/Login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
